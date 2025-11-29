939f4833632aafd04715ca98b3b34435eadf0749
Ramashis2004 <dasramashsiskalyan@gamil.com>
2025-11-29 09:38:19 +0530

    first commit

---
+++ models/districtEventModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const districtEventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, trim: true },
+    date: { type: Date },
+    venue: { type: String, trim: true },
+    gender: { type: String, enum: ["boy", "girl", "both"], default: "both" },
+  },
+  { timestamps: true }
+);
+
+districtEventSchema.index({ title: 1 }, { unique: true });
+
+module.exports = mongoose.model("DistrictEvent", districtEventSchema);
